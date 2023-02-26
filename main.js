05b0283143f8e8ae85dda94880a966cd9d310ec1
gogolashvili <ggogolashvili91@gmail.com>
2023-02-26 12:48:03 +0400

    add box styles

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ container.style.overflow = "auto";
 container.style.display = "flex";
 container.style.justifyContent = "center";
 container.style.alignItems = "center";
+
+let box = document.createElement("div");
+container.appendChild(box);
+box.style.width = "800px";
+box.style.height = "500px";
+box.style.overflow = "auto";
