dcc47b83f4c9b4a8b728e7e608eaba9b99cc331d
gogolashvili <ggogolashvili91@gmail.com>
2023-02-26 12:50:57 +0400

    add div styles

--- main.js
+++ main.js
@@ -23,3 +23,18 @@ satauri.style.color = "white";
 satauri.style.fontSize = "50px";
 satauri.style.marginTop = "30px";
 satauri.style.marginLeft = "20%";
+
+let part2 = document.createElement("div");
+box.appendChild(part2);
+part2.style.backgroundColor = "#F3E13D";
+part2.style.width = "600px";
+part2.style.height = "300px";
+part2.style.borderRadius = "30px";
+part2.style.marginTop = "60px";
+part2.style.marginLeft = "15%";
+part2.style.overflow = "auto";
+part2.style.display = "flex";
+part2.style.flexDirection = "column";
+part2.style.justifyContent = "center";
+part2.style.alignItems = "center";
+part2.style.gap = "30px";
