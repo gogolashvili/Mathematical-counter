652183e21ffc97183eec0a04c63f2b8efc51b9eb
gogolashvili <ggogolashvili91@gmail.com>
2023-02-26 12:47:17 +0400

    add body styles

---
+++ main.js
@@ -0,0 +1,9 @@
+let container = document.createElement("div");
+document.body.appendChild(container);
+container.style.width = "100vw";
+container.style.height = "100vh";
+container.style.backgroundColor = "rgb(45, 149, 351)";
+container.style.overflow = "auto";
+container.style.display = "flex";
+container.style.justifyContent = "center";
+container.style.alignItems = "center";
